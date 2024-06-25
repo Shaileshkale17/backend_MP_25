0ad0c9edd5472a19f2b546920a5eeb4cccefbc9d
“shailesh <shaileshkale87730@gmail.com>
2024-06-25 18:15:33 +0530

    first commit

---
+++ Controllers/customerController.js
@@ -0,0 +1,57 @@
+import Customer from '../Models/customer.js';
+import asyncHandler from '../utils/asyncHandler.js';
+import { v4 as uuidv4 } from 'uuid';
+
+
+export const createCustomer = asyncHandler(async (req, res) => {
+    const { name, email, mobile, projectName, projectLocation  } = req.body;
+    const emailFound = await Customer.findOne({email});
+    if (emailFound){
+        return res.status(400).json({ message: 'This customer already exits.' });
+    }
+    const customerId = uuidv4();
+    Customer.create({
+        name, 
+        email, 
+        mobile, 
+        projectName, 
+        projectLocation, 
+        customerId
+    })
+    res.status(201).json({
+        name, 
+        email, 
+        mobile, 
+        projectName, 
+        projectLocation, 
+        customerId
+    })
+});
+
+
+export const getCustomers = asyncHandler(async (req, res) => {
+    const customers = await Customer.find();
+    res.status(200).json(customers);
+});
+
+
+export const getCustomerById = asyncHandler(async (req, res) => {
+    const registeredCustomer = await Customer.find({"customerId" : req.params.id});
+    if (!registeredCustomer) return res.status(404).json({ message: 'Customer not found' });
+    res.status(200).json(registeredCustomer);
+});
+
+
+export const updateCustomer = asyncHandler(async (req, res) => {
+    const { name, email, mobile } = req.body;
+    const customer = await Customer.findByIdAndUpdate(req.params.id, { name, email, mobile }, { new: true, runValidators: true });
+    if (!customer) return res.status(404).json({ message: 'Customer not found' });
+    res.status(200).json(customer);
+});
+
+
+export const deleteCustomer = asyncHandler(async (req, res) => {
+    const customer = await Customer.findByIdAndDelete(req.params.id);
+    if (!customer) return res.status(404).json({ message: 'Customer not found' });
+    res.status(200).json({ message: 'Customer deleted' });
+});
---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import partnerRouter from "./router/partnerRouter.js";
+import customerRoutes from "./router/customerRoutes.js";
+import cors from "cors";
+import logger from "./Middlewares/logger.js";
+import errorMiddleware from "./Middlewares/errorMiddleware.js";
+import authRoutes from "./router/authRoutes.js";
+import bodyParser from "body-parser";
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(logger);
+app.use(errorMiddleware);
+
+app.use("/api/partners", partnerRouter);
+app.use("/api/customers", customerRoutes);
+app.use("/api/login", authRoutes);
+
+export { app };
